28355f25ca6c4ea88e3cb739f82f93d1e74fd914
dheim123 <dheim@midland.k12.ia.us>
2026-04-23 12:32:35 -0500

    Refactor Game class for improved readability and cheats

--- Game.cs
+++ Game.cs
@@ -1,6 +1,10 @@
+
 class Game
 {
     private string phrase;
+
+    // You can change the magic word if you want
+    private const string magicWord = "PLAY";
 
     public Game(string inputPhrase)
     {
@@ -10,76 +14,99 @@ class Game
 
     public string DisplayBlanks()
     {
-        // This will hold the result we build and return
-        string result = "";
-
-        // Go through each letter in the phrase one at a time
-        foreach (char letter in phrase)
-        {
-            // If the character is a space, keep spacing between words
-            if (letter == ' ')
-            {
-                result += "  "; // double space between words
-            }
-            else
-            {
-                // Otherwise, show an underscore for each hidden letter
-                result += "_ ";
-            }
-        }
-
-        // Return the finished blank version of the phrase
-        return result;
-    }
-
-    public string Play(char[] guessedLetters)
-    {
-        // Convert the char array into a string and make it uppercase
-        // This makes it easier to compare letters later
-        string guesses = new string(guessedLetters).ToUpper();
-
-        // This will hold the result we build and return
         string result = "";
 
         // Go through each letter in the phrase
         foreach (char letter in phrase)
         {
-            // If it's a space, keep it
             if (letter == ' ')
             {
                 result += "  ";
             }
-            // If the guessed letters contain this letter, reveal it
-            else if (guesses.Contains(letter))
-            {
-                result += letter + " ";
-            }
             else
             {
-                // Otherwise, keep it hidden
                 result += "_ ";
             }
         }
 
-        // Return the updated phrase showing guessed letters
+        return result;
+    }
+
+    public string Play(char[] guessedLetters)
+    {
+        // Convert guesses to a string and uppercase
+        string guesses = new string(guessedLetters).ToUpper();
+
+        // Check if cheats are active
+        bool timeCheat = ApplyTimeCheat(DateTime.Now, 2);
+        bool easterEgg = ApplyEasterEggCheat(guesses);
+
+        string result = "";
+
+        // Split the phrase into words (so we can reveal whole words)
+        string[] words = phrase.Split(' ');
+
+        // Loop through each word
+        for (int w = 0; w < words.Length; w++)
+        {
+            // Add space between words
+            if (w > 0)
+            {
+                result += "  ";
+            }
+
+            // Time cheat: reveal every other word starting at 2nd word
+            bool revealWholeWord = easterEgg || (timeCheat && w % 2 == 1);
+
+            // Loop through each letter in the word
+            foreach (char letter in words[w])
+            {
+                if (revealWholeWord)
+                {
+                    // Show entire word if cheat is active
+                    result += letter + " ";
+                }
+                else if (guesses.Contains(letter))
+                {
+                    // Show guessed letters
+                    result += letter + " ";
+                }
+                else
+                {
+                    // Otherwise hide letter
+                    result += "_ ";
+                }
+            }
+        }
+
         return result;
     }
 
     public bool IsValid(string guessedLetters)
     {
-        // Must be exactly 10 characters
         if (guessedLetters.Length != 10)
         {
             return false;
         }
 
-        // Must not contain any spaces
         if (guessedLetters.Contains(" "))
         {
             return false;
         }
 
-        // If both checks pass, the input is valid
         return true;
     }
+
+    public bool ApplyTimeCheat(DateTime now, int divisibleByValue)
+    {
+        // Returns true if the current second is divisible by the value
+        return now.Second % divisibleByValue == 0;
+    }
+
+    public bool ApplyEasterEggCheat(string guessedLetters)
+    {
+        // Returns true if the magic word is inside the guesses
+        return guessedLetters.Contains(magicWord);
+    }
 }
+
